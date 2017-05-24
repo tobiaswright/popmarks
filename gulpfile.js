7067d94b6502ae2c4386255fc187fec438b7e68a
Tobias Wright <tobias.wright@gmail.com>
2017-05-24 09:53:26 -0400

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
 gulp.task('default', function() {
-  return gulp.src('src/entry.js')
-    .pipe(webpack())
-    .pipe(gulp.dest('dist/'));
+return gulp.src('src/app.js')
+  .pipe(webpack( require('./webpack.config.js') ))
+  .pipe(gulp.dest('dist/'));
 });
