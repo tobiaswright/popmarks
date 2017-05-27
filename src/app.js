60b89be0a089a02c56c68d8e2a7a6f1c1387094f
Tobias Wright <tobias.wright@gmail.com>
2017-05-27 09:38:05 -0400

    added fetch api

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("alloo")
+console.log("alloo") 
+
+let url = "http://www.blackenterprise.com/technology/new-program-molds-hbcu-students-into-tech-investors/"
+
+fetch('http://localhost:7071/api/getPageInfo?url=' + url)
+.then((response) => {
+    response.json().then(function(data) {  
+        console.log(data);  
+    }); 
+});
+
+console.log("alloo")    
