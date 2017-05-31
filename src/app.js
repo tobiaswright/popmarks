903ee38fbae67faa9bf19635018fa56251fa4ea2
Tobias Wright <tobias.wright@gmail.com>
2017-05-30 23:58:17 -0400

    added form input

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
-let url = "http://www.blackenterprise.com/technology/new-program-molds-hbcu-students-into-tech-investors/"
+const submit =  document.getElementById("submit");
+const formurl = document.getElementById("formurl");
 
-fetch('http://localhost:7071/api/getPageInfo?url=' + url)
-.then((response) => {
-    response.json().then(function(data) {  
-        console.log(data);  
-    }); 
-});
+let retrieveInput = () => {
+    let formVal = formurl.value;
+    formurl.value = "";
+    return formVal;
+}
+
+let url = retrieveInput();
+
+submit.addEventListener("click", (url, e) => {
+    e.preventDefault();
+
+    console.log("alloooo");
+    fetch('http://localhost:7071/api/getPageInfo?url=' + url)
+    .then((response) => {
+        response.json().then(function(data) {  
+            console.log(data);  
+        }); 
+    });
+}, false);
+
