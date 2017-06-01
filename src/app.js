76cd421dc21d3c9d43d24d9aaa871db1acbac831
Tobias Wright <tobias.wright@gmail.com>
2017-06-01 03:02:16 -0400

    insert url info into list

--- src/app.js
+++ src/app.js
@@ -15,10 +15,15 @@ let returnURLInfo = (e) => {
     fetch('http://localhost:7071/api/getPageInfo?url=' + url)
     .then((response) => {
         response.json().then(function(data) {  
-            console.log(data);  
+            console.log(data);
+            let newItem = document.createElement("li");       // Create a <li> node
+            let textnode = document.createTextNode(data.data.ogTitle);  // Create a text node
+            newItem.appendChild(textnode);                    // Append the text to <li>
+
+            let list = document.getElementById("main");    // Get the <ul> element to insert a new node
+            list.insertBefore(newItem, list.childNodes[0]);  // Insert <li> before the first child of <ul>
         }); 
     });
 }
 
 submit.addEventListener("click", returnURLInfo, false);
-
