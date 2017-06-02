781834c9d8d976c430d04cf41b63669f6ac5a6ba
Tobias Wright <tobias.wright@gmail.com>
2017-06-02 12:28:43 -0400

    refactored insert

--- src/app.js
+++ src/app.js
@@ -7,6 +7,16 @@ let retrieveInput = () => {
     return formVal;
 }
 
+let insertData = (data) => {
+    console.log(data);
+    let newItem = document.createElement("li");       // Create a <li> node
+    let textnode = document.createTextNode(data.data.                ogTitle);  // Create a text node
+    newItem.appendChild(textnode);                    // Append the text to <li>
+
+    let list = document.getElementById("main");    // Get the <ul> element to insert a new node
+    list.insertBefore(newItem, list.childNodes[0]);  // Insert <li> before the first child of <ul>
+}
+
 let returnURLInfo = (e) => {
     e.preventDefault();
 
@@ -14,15 +24,7 @@ let returnURLInfo = (e) => {
 
     fetch('http://localhost:7071/api/getPageInfo?url=' + url)
     .then((response) => {
-        response.json().then(function(data) {  
-            console.log(data);
-            let newItem = document.createElement("li");       // Create a <li> node
-            let textnode = document.createTextNode(data.data.ogTitle);  // Create a text node
-            newItem.appendChild(textnode);                    // Append the text to <li>
-
-            let list = document.getElementById("main");    // Get the <ul> element to insert a new node
-            list.insertBefore(newItem, list.childNodes[0]);  // Insert <li> before the first child of <ul>
-        }); 
+        response.json().then(insertData); 
     });
 }
 
